7b638f9dce71f9e751762e85bf513620a7e7b9c8
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-02 11:05:46 -0300

    Vimos los tipos de variables y como definirlas

---
+++ js/01-variables.js
@@ -0,0 +1,33 @@
+//Comentario simple
+
+/*
+comentario
+multilinea
+*/
+
+//Declaracion de variables (let - const - var)
+
+let nombre = "Nacho";
+const url = "https://universe.rollingcodeschool.com/";
+
+//mostrar mensaje en consola
+
+console.log("Hola Mundo");
+console.log("Contenido de la variable nombre: " +nombre);
+
+//modificar el valor de la variable
+
+nombre = "Ignacio";
+console.log("Contenido de la variable nombre: " +nombre);
+console.log(url);
+
+//tipo de datos
+
+const apellido = "Holmquist"; //string
+let edad = 30; //number
+let decimal = 30.4; //number
+let negativo = -30; //number
+const vacio = null; //null
+let dato; //undefined
+let encendido = true; //boolean
+let apagado = false; //boolean
