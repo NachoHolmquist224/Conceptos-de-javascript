9b809e6927defa22fa4c3a1210d9c3a562f32813
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-09 16:23:29 -0300

    Hacemos el ejercicio de multiplicar un numero por diez

---
+++ js/ejercicios/multiplicarnumero.js
@@ -0,0 +1,6 @@
+const num = prompt("Ingresa tu numero favorito")
+const diez = 10;
+const res = parseInt(num) * diez;
+console.log(`El resultado de la multiplicacion es: ${res}`);
+
+document.writeln(`<p>El resultado de la multiplicacion es: ${res}</p>`)
