bf1a2f9492c2cd3d7dfb35df17e337b6a84d609a
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-09 16:27:54 -0300

    Hacemos el ejercicio de convertir los grados

---
+++ js/ejercicios/convertirgrados.js
@@ -0,0 +1,5 @@
+const fahrenheit = prompt("Ingresa la temperatura en F°");
+const celcius = (5/9)*(fahrenheit-32)
+console.log(`La temperatura en C° es: ${celcius}`);
+
+document.writeln(`<p>La temperatura en C° es: ${celcius}</p>`)
