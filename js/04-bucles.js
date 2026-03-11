aa4fd9b8e3d118a5c26fee668b9a1d389dca176b
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-11 16:33:35 -0300

    vimos el bucle for con un ejemplo

--- js/04-bucles.js
+++ js/04-bucles.js
@@ -1,13 +1,17 @@
 const pantalla = document.getElementById("pantalla")
-let contador = 1
+// let contador = 1
 
 // while(contador <= 500){
 //     pantalla.innerHTML += `<p>Fila ${contador}</p>`
 //     contador++;
 // }
 
-do{
+// do{
+//     pantalla.innerHTML += `<p>Fila ${contador}</p>`
+//     contador++;
+// }
+// while(contador <= 500)
+
+for(let contador = 1; contador<=500; contador++){
     pantalla.innerHTML += `<p>Fila ${contador}</p>`
-    contador++;
 }
-while(contador <= 500)
