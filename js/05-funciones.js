e5c1b477efd7320c406b9d963b9acee01c4e57e2
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-16 11:11:34 -0300

    Agregamos ejemplos de los 3 tipos de funciones

---
+++ js/05-funciones.js
@@ -0,0 +1,41 @@
+// función tradicional sin parametros
+
+function saludar(){
+    const pantalla = document.getElementById("pantalla")
+    pantalla.innerHTML += "<p>Hola Mundo 🌎</p>"
+}
+
+// funcion con parámetros
+
+function saludarpersona(nombre, edad){
+    const pantalla = document.getElementById("pantalla")
+    pantalla.innerHTML += `<p>Hola ${nombre}</p>`
+    console.log("Edad:" + edad)
+}
+
+// funcion con retorno
+
+function aplicardescuento(precio, descuento){
+    const preciofinal = precio - (precio * (descuento / 100))
+    return preciofinal
+    // !NO SE AGREGA MAS CODIGO DEBAJO DE UN RETURN
+}
+
+// invocar o llamar a la función
+saludar()
+saludar()
+saludar()
+
+const nombre = prompt("Ingrese su nombre")
+const edad = parseInt(prompt("Ingrese su edad"))
+const precio = parseFloat(prompt("Ingrese el precio del producto"))
+const descuento = parseFloat(prompt("Ingrese el descuento del producto"))
+
+saludarpersona(nombre, edad)
+
+aplicardescuento(precio, descuento)
+
+const preciofinal = aplicardescuento(precio, descuento)
+
+const pantalla = document.getElementById("pantalla")
+pantalla.innerHTML += `<p>El precio del producto es $${precio}, con el descuento del ${descuento}%, el precio final es de $${preciofinal}</p>`
