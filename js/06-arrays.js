01294083e16a2e4c2d4b7bb751e4a003d35cea0b
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-20 23:37:53 -0300

    Operaciones normales en un array (agg, modificar, eliminar y filtrar elementos del mismo)

--- js/06-arrays.js
+++ js/06-arrays.js
@@ -1,48 +1,75 @@
+function mostrarpeliculas(nota) {
+  pantalla.innerHTML += `<h2>Lista de Peliculas (${peliculas.length})</h2>`;
+  pantalla.innerHTML += `<p>Notas: (${nota})</p>`;
+
+  lista = "<ul>";
+  for (i = 0; i < peliculas.length; i++) {
+    lista += `<li>${i}- ${peliculas[i]}</li>`;
+  }
+  lista += "/<ul>";
+
+  pantalla.innerHTML += lista;
+}
+
 // Como declarar un array vacío
 
 const array = [];
 
 // Como declarar un array con datos
 
-const arraycondatos = [1, 2, "tres", 4.5, "cinco"];
+const peliculas = ["Rapido y Furioso", "Shrek", "Shrek 2", "Shazam", "Venom", "El lorax"];
 
-console.log(typeof arraycondatos)
+console.log(typeof peliculas);
 
 // Cuantos elementos tiene un array
 
-const pantalla = document.getElementById("pantalla")
-pantalla.innerHTML = `<p>El array tiene ${arraycondatos.length} elementos</p>`
+const pantalla = document.getElementById("pantalla");
+pantalla.innerHTML = `<p>El array tiene ${peliculas.length} peliculas</p>`;
 
 // Como no mostrar un array
 
-pantalla.innerHTML += arraycondatos
+pantalla.innerHTML += peliculas;
 
 // Mostrar los elementos de un array en lista
+mostrarpeliculas("Array original");
 
-pantalla.innerHTML += "<h2>Lista de elementos (5)</h2>"
+// Acceder a un elemento de un array
 
-let lista = "<ul>"
-for(i=0; i<arraycondatos.length; i++){
-    lista += `<li>${arraycondatos[i]}</li>`
-}
-lista += "/<ul>" 
+pantalla.innerHTML += `El elemento 3 del array es: ${peliculas[2]}`;
 
-pantalla.innerHTML += lista
-
-// Formas de acceder a un elemento de un array
-
-pantalla.innerHTML += `El elemento 3 del array es: ${arraycondatos[2]}`
+// Operaciones normales en un array
 
 // Agregar un elemento a un array
 
-arraycondatos.unshift("23")
+peliculas.unshift("La vida es bella");
+mostrarpeliculas("Agregamos La vida es bella al principio del array con unshift");
 
-pantalla.innerHTML += "<h2>Lista de elementos (6)</h2>"
+peliculas.push("Matilda");
+mostrarpeliculas("Agregamos Matilda al final del array con push");
 
-lista = "<ul>"
-for(i=0; i<arraycondatos.length; i++){
-    lista += `<li>${arraycondatos[i]}</li>`
-}
-lista += "/<ul>" 
+peliculas.splice(4,0, "El espanta tiburones");
+mostrarpeliculas("Agregamos El espanta tiburones en la posicion 4 del array con splice");
 
-pantalla.innerHTML += lista
+// Modificar elementos de un array
+
+peliculas[3] = "Intensamente"
+mostrarpeliculas("Modificamos el elemento 3 del array")
+
+// Eliminar elementos de un array
+
+peliculas.shift()
+mostrarpeliculas("Eliminamos el primer elemento del array con shift")
+
+peliculas.pop()
+mostrarpeliculas("Eliminamos el ultimo elemento del array con pop")
+
+peliculas.splice(3,1) // Si no le indicamos la cantidad borra todo desde la posicion 3
+mostrarpeliculas("Eliminamos el elemento 3 del array con splice")
+
+// Filtrar elementos de un array
+
+console.log(peliculas[1].includes("Shrek"));
+console.log(peliculas[1].includes("Shrek"));
+
+const sagashrek = peliculas.filter((pelicula)=> pelicula.includes("Shrek"));
+console.log(sagashrek);
