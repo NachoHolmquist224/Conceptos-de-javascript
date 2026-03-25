fac9f2c057acb21c0b0162074199fec0d913911a
Ignacio Holmquist <ignacioholmquist12@gmail.com>
2026-03-25 10:26:42 -0300

    Modificamos las variables por string en la propiedad Elenco y ya se muestra bien el objeto por consola

--- js/07-objetos.js
+++ js/07-objetos.js
@@ -13,7 +13,7 @@ const pelicula = {
     titulo : "Super Mario Bros",
     duración : "2:30:00",
     genero : "Infantil",
-    elenco : [actor1, actor2, actor3],    
+    elenco : ["actor1", "actor2", "actor3"],    
     añoestreno : 2023,
     //metodos
     reproducir : function(){
@@ -25,3 +25,34 @@ const pelicula = {
 }
 
 console.log(pelicula);
+const pantalla = document.getElementById("pantalla");
+pantalla.innerHTML = `<p>${pelicula}</p>`
+
+// Como mostrar las propiedades de un objeto
+
+pantalla.innerHTML = `<p>Titulo: ${pelicula.titulo}</p>`
+pantalla.innerHTML += `<p>Duración: ${pelicula.duración}</p>`
+pantalla.innerHTML += `<p>Genero: ${pelicula.genero}</p>`
+pantalla.innerHTML += `<p>Año de estreno: ${pelicula.añoestreno}</p>`
+
+// Como mostrar todas las propiedades de un objeto (for.....in)
+pantalla.innerHTML += "<h2>Propiedades del objeto</h2>"
+
+let propiedadespelicula;
+
+for(const propiedad in pelicula){
+    const valor = pelicula[propiedad]
+    propiedadespelicula += `<p>${propiedad}: ${valor}</p>`
+}
+
+pantalla.innerHTML += propiedadespelicula
+
+// Agregar una propiedad al objeto
+
+pelicula.estado = true;
+pantalla.innerHTML += `<p>Estado: ${pelicula.estado}</p>`
+
+// Modificar una propiedad del objeto
+
+pelicula.estado = false;
+pantalla.innerHTML += `<p>Estado: ${pelicula.estado}</p>`
